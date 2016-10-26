182da9aa2592f0c515c346541ea76826b544b470
Paul Sohn <pksohn@gmail.com>
2016-10-25 18:12:20 -0700

    Working classes for prediction and simulation

--- gaussian.py
+++ gaussian.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import spatial
+import matplotlib.pyplot as plt
 
 
 def covariance(x, y, l):
@@ -25,35 +26,44 @@ class SimpleKriging(object):
         self.X = training_data[:, :-1]
         self.Y = training_data[:, -1:]
 
-    def predict(self, test_data, l, indices=False):
+    def predict(self, test_data, l, sigma, indices=False):
 
         K_xtest_x = covariance(test_data, self.X, l)
         K = covariance(self.X, self.X, l)
 
-        sigma_sq_I = np.var(self.Y) * np.eye(len(self.X))
+        sigma_sq_I = sigma**2 * np.eye(len(self.X))
         inv = np.linalg.inv(K + sigma_sq_I)
 
         predictions = K_xtest_x.dot(inv).dot(self.Y)
 
         if indices:
-            return np.concatenate([test_data, predictions], axis=1)
+            return np.concatenate((test_data, predictions), axis=1)
         else:
             return predictions
 
-
-    def simulate(self, bbox, ncells, l, gamma=0.001, indices=False):
+    def simulate(self, bbox, ncells, l, sigma, gamma=0.001, indices=False, show_visual=False, save_visual=None):
 
         grid = make_grid(bounding_box=bbox, ncell=ncells)
 
-        prediction = self.predict(test_data=grid, l=l, indices=True)
+        prediction = self.predict(test_data=grid, l=l, sigma=sigma, indices=True)
 
-        K = covariance(x=self.X, y=self.X, l=l)
-        L = np.linalg.cholesky(K + gamma * np.eye(K.shape[0]))
+        K = covariance(grid, grid, l)
+        L = np.linalg.cholesky(K + gamma * np.eye(len(K)))
+        u = np.random.normal(size=len(L))
 
-        result = prediction + L
+        result = prediction[:, -1] + L.dot(u)
+
+        if show_visual or save_visual:
+            x = grid[:, 0]
+            y = grid[:, 1]
+            plt.scatter(x , y, c=result)
+            plt.colorbar(ticks=[np.min(result), np.max(result)], label='Rainfall in mm')
+            if show_visual:
+                plt.show()
+            if save_visual:
+                plt.savefig(save_visual)
 
         if indices:
-            return np.concatenate([grid, result], axis=1)
+            return np.concatenate((grid, result[:, None]), axis=1)
         else:
             return result
-
