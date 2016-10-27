59e3ef4782c3525274f62da5c2d1362e22e302f8
Paul Sohn <pksohn@gmail.com>
2016-10-26 17:14:48 -0700

    Docstrings

--- gaussian.py
+++ gaussian.py
@@ -5,18 +5,66 @@ import simplekml
 
 
 def sq_exponential(x, y, l):
+    """
+    Squared exponential covariance function.
+
+    Parameters
+    ----------
+    x : Array-like
+        First array of coordinates
+    y : Array-like
+        Second array of coordinates
+    l : int or float
+        Length-scale value
+
+    Returns
+    -------
+    Covariance matrix
+
+    """
     d = spatial.distance_matrix(x, y)
     k = np.exp(-(d ** 2) / (2 * l * l))
     return k
 
 
 def exponential(x, y, l):
+    """
+    Exponential covariance function.
+
+    Parameters
+    ----------
+     x : Array-like
+        First array of coordinates
+    y : Array-like
+        Second array of coordinates
+    l : int or float
+        Length-scale value
+
+    Returns
+    -------
+    Covariance matrix
+
+    """
     d = spatial.distance_matrix(x, y)
     k = np.exp(-d/l)
     return k
 
 
 def make_grid(bounding_box, ncell):
+    """
+
+    Parameters
+    ----------
+    bounding_box : List or tuple
+        The bounding box to make a grid within. Must include four elements.
+    ncell : int
+        The number of cells to create along one dimension (if you want a 10x10 grid, pass 10 to this parameter)
+
+    Returns
+    -------
+    NDArray
+
+    """
     xmax, xmin, ymax, ymin = bounding_box
     xgrid = np.linspace(xmin, xmax, ncell)
     ygrid = np.linspace(ymin, ymax, ncell)
@@ -27,6 +75,31 @@ def make_grid(bounding_box, ncell):
 
 
 def cross_validate(train, l_values, sigma_values, rmse_opt, k_folds, cov_funcs=False, verbose=False):
+    """
+    K-fold cross validation for the Gaussian prediction model.
+
+    Parameters
+    ----------
+    train : Array-like
+        Training dataset to use for cross-validation
+    l_values : Array-like or numeric
+        Array of values of l (length-scale) to try, or single value
+    sigma_values : Array-like or numeric
+        Array of values of sigma (noise) to try, or single value
+    rmse_opt : int or float
+        Starting value for root mean squared error (RMSE)
+    k_folds : int
+        Number of folds to use for cross-validation
+    cov_funcs : bool, optional
+        Tests multiple covariance functions if True (for now, only basic exponential function)
+    verbose : bool, optional
+        If True, prints RMSE for each combination of values
+
+    Returns
+    -------
+    l_opt, sigma_opt, cov_func_final, rmse_opt
+
+    """
     if not isinstance(l_values, (list, tuple, np.ndarray, np.array)):
         l_values = list(l_values)
 
@@ -69,6 +142,10 @@ def cross_validate(train, l_values, sigma_values, rmse_opt, k_folds, cov_funcs=F
 
 
 class SimpleKriging(object):
+    """
+    Model object for predicting and simulating values in two dimensions based on a training dataset based on
+    the simple Kriging method (using an underlying Gaussian process).
+    """
 
     def __init__(self, training_data):
         self.training_data = training_data
@@ -76,6 +153,25 @@ class SimpleKriging(object):
         self.Y = training_data[:, -1:]
 
     def predict(self, test_data, l, sigma, indices=False, cov_function=sq_exponential):
+        """
+
+        Parameters
+        ----------
+        test_data : Array-like
+            Set of coordinates to predict values for
+        l : int or float
+            Length-scale value
+        sigma : int or float
+            Noise parameter
+        indices : bool, optional
+            If True, returns results with coordinates.
+        cov_function : function, optional
+            Uses squared exponential by default. Pass a different covariance function here if desired.
+
+        Returns
+        -------
+
+        """
 
         K_xtest_x = cov_function(test_data, self.X, l)
         K = cov_function(self.X, self.X, l)
@@ -92,6 +188,29 @@ class SimpleKriging(object):
 
     def simulate(self, bbox, ncells, l, sigma, gamma=0.001, indices=False, cov_function=sq_exponential,
                  show_visual=False, save_kml=None):
+        """
+
+        Parameters
+        ----------
+        bbox : Array-like
+            Bounding box of coordinates to create grid within. Must contain four values.
+        ncells : int
+            The number of cells to create along one dimension (if you want a 10x10 grid, pass 10 to this parameter)
+        l : int or float
+            Length-scale value
+        sigma : int or float
+            Noise parameter
+        gamma : int or float, optional
+            Value for numerical stabilization to add to Cholesky decomposition
+        indices : bool, optional
+            If True, returns results with coordinates.
+        cov_function : function, optional
+            Uses squared exponential by default. Pass a different covariance function here if desired.
+        show_visual : bool, optional
+            Displays visualized results if True
+        save_kml : str, optional
+            Saves KML file to specified filename (without extension) if this argument is passed.
+        """
 
         grid = make_grid(bounding_box=bbox, ncell=ncells)
 
